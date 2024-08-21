7ccadd5540a449b597b3cdbc8bcbe1046e41f71e
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-08-21 19:24:39 -0300

    commit

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
@@ -6,10 +7,14 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
-
+import uploadConfig from './config/multer'
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 router.post('/users', new CreateUserController().handle)
 
@@ -20,5 +25,11 @@ router.get ('/me', isAuthenticated, new DetailUserController().handle)
 //Routas categories
 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+//Rotas Product
+
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
 
 export { router };
